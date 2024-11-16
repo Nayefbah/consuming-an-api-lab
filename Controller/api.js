d4bf9ccf86fc7e000536cbee27bc7d0647e0719e
Nayefbah <nayef.rind@gmail.com>
2024-11-16 13:56:02 +0300

    work Done with style

--- Controller/api.js
+++ Controller/api.js
@@ -1,21 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const router = express.Router();
-router.use(express.urlencoded({ extended: true }));
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+router.use(express.urlencoded({ extended: true }))
 
 router.post('/weather', (req, res) => {
-  
   const zipCode = req.body.ZipCode
   const apiKey = process.env.API_KEY
   const URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${apiKey}`
-
   axios
     .get(URL)
     .then((response) => {
       const weatherData = response.data
-    res.render('weather/show.ejs', {data: weatherData})
-      
+      res.render('weather/show.ejs', { data: weatherData })
     })
     .catch((err) => {
       console.error('Error fetching weather data:', err)
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
 const apiRouter = require(`./Controller/api`)
 const port = 4000
-
+app.use(express.static(path.join(__dirname)))
 app.use(`/`, apiRouter)
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
